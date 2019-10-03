6d4a2540013640943e992676599fba1b4f9b914a
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-10-03 22:37:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,19 +1,19 @@
 require "pry"
 
 class Transfer
-  
+
 attr_accessor :sender, :receiver, :amount, :status, :balance
 #attr_reader :accounts
 
 def initialize(sender, receiver, amount)
-  @sender = sender 
+  @sender = sender
   @receiver = receiver
   @amount = amount
   @status = "pending"
 end
 
 def valid?
-  sender.valid? && receiver.valid? 
+  sender.valid? && receiver.valid?
 end
 
 def execute_transaction
@@ -21,15 +21,15 @@ def execute_transaction
     receiver.balance = receiver.balance + amount
     sender.balance = sender.balance - amount
     @status = "complete"
-  else 
+  else
     @status = "rejected"
     return "Transaction rejected. Please check your account balance."
     end
-    #return @status 
+    #return @status
     #binding.pry
 end
 
-def reverse_transfer 
+def reverse_transfer
   if @status == "complete"
     receiver.balance = receiver.balance - amount
     sender.balance = sender.balance + amount
